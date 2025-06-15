c83dc6864c7c8460eacb2ec433d3c85c7a917a50
ALEXSTONE <solo-drive@bk.ru>
2025-06-15 20:49:22 +0300

    hw_38 - создал проект ,добавил файл в giignore запустил тестовый запуск!

---
+++ barbershop/urls.py
@@ -0,0 +1,12 @@
+# barbershop\urls.py
+from django.contrib import admin
+from django.urls import path
+from core.views import landing, thanks, orders_list, order_details
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', landing),
+    path('thanks/', thanks),
+    path('orders/', orders_list),
+    path('orders/<int:order_id>/', order_details),
+]
---
+++ core/views.py
@@ -0,0 +1,40 @@
+# core/views.py
+from django.shortcuts import render, HttpResponse
+from .data import orders, services, masters
+
+def landing(request):
+    """
+    Отвечает за маршрут '/'
+    """
+    return  HttpResponse('<h1>Главная страница</h1>')
+
+def thanks(request):
+    """
+    Отвечает за маршрут  '/thanks/'
+    """
+    return render(request, 'thanks.html')
+
+def orders_list(request):
+    """
+    Отвечает за маршрут /orders/'
+    """
+    return HttpResponse('<h1>Список заказов</h1>')
+
+def order_details(request, order_id):
+    """
+    Отвечает за маршрут /orders/<int:order_id>/
+    :param request: HttpRequest
+    :param order_id: int (номер заказа)
+    """
+    order = [order for order in orders if order['id'] == order_id]
+    try:
+        order = order[0]
+        context = {
+            "order": order,
+            "my_fariable": "Hello, world",
+        }
+    except IndexError:
+        return HttpResponse(f'<h1>Заказ {order_id} не найден</h1>')
+    else:
+        
+        return render(request, 'order_details.html', context=context)
