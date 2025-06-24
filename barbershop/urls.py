0189312e3aa26230ba308b265f9ec3e7d9222588
ALEXSTONE <solo-drive@bk.ru>
2025-06-24 22:21:47 +0300

    hw_39 - начало новой домашки!

--- barbershop/urls.py
+++ barbershop/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from core.views import landing, thanks, orders_list, order_detail
+from core.views import landing, thanks, orders_list, order_detail, about, services, all_masters, appointment
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,4 +11,8 @@ urlpatterns = [
     path('thanks/', thanks, name='thanks'),
     path('orders/', orders_list, name='orders_list'),
     path('orders/<int:order_id>/', order_detail, name='order_detail'),
+    path('about/', about, name='about'),
+    path('services/', services, name='services'),
+    path('masters/', all_masters, name='masters'),
+    path('appointment/', appointment, name='appointment')
 ] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
--- core/views.py
+++ core/views.py
@@ -1,12 +1,38 @@
-# core/views.py
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 from .data import orders, services, masters
 
+def about(request):
+    return render(request, 'about.html')
+
+def services(request):
+    services_with_masters = [
+        {"name": "Стрижка под 'Горшок'", "masters": [masters[0]]},
+        {"name": "Укладка 'Взрыв на макаронной фабрике'", "masters": [masters[1]]},
+        {"name": "Королевское бритье опасной бритвой", "masters": [masters[2]]},
+        {"name": "Окрашивание 'Жизнь в розовом цвете'", "masters": [masters[3]]},
+        {"name": "Мытье головы 'Душ впечатлений'", "masters": [masters[4]]},
+        {"name": "Стрижка бороды 'Боярин'", "masters": [masters[0], masters[2]]},
+        {"name": "Массаж головы 'Озарение'", "masters": [masters[1], masters[3]]},
+        {"name": "Укладка 'Ветер в голове'", "masters": [masters[4]]},
+        {"name": "Плетение косичек 'Викинг'", "masters": [masters[0], masters[3]]},
+        {"name": "Полировка лысины до блеска", "masters": [masters[2]]},
+    ]
+
+    context = {
+        'services_with_masters': services_with_masters,
+    }
+    return render(request, 'services.html', context=context)
+
+def all_masters(request):
+    context = {
+        'masters': masters,
+    }
+    return render(request, 'masters.html', context=context)
+
+def appointment(request):
+    return render(request, 'appointment.html')
+
 def landing(request):
-    """
-    Отвечает за маршрут '/'
-    """
-    # Создаем структуру данных для услуг и мастеров
     services_with_masters = [
         {"name": "Стрижка под 'Горшок'", "masters": [masters[0]]},
         {"name": "Укладка 'Взрыв на макаронной фабрике'", "masters": [masters[1]]},
@@ -27,31 +53,19 @@ def landing(request):
     return render(request, 'landing.html', context)
 
 def thanks(request):
-    """
-    Отвечает за маршрут  '/thanks/'
-    """
     return render(request, 'thanks.html')
 
 def orders_list(request):
-    """
-    Отвечает за маршрут /orders/'
-    """
     context = {
         "orders": orders,
     }
     return render(request, 'orders_list.html', context=context)
 
 def order_detail(request, order_id):
-    """
-    Отвечает за маршрут /orders/<int:order_id>/
-    :param request: HttpRequest
-    :param order_id: int (номер заказа)
-    """
-    order = next((order for order in orders if order['id'] == order_id), None) 
-    # Использует next для поиска заказа по order_id, что делает код более читаемым.
+    order = next((order for order in orders if order['id'] == order_id), None)
     if order is None:
-        return render (request, "order_not_found.html", {"order_id": order_id}, status=404)
-    
+        return render(request, "order_not_found.html", {"order_id": order_id}, status=404)
+
     context = {
         "order": order,
     }
