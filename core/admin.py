61b7c0b695d5663b81b6cf82042da8b858c057ac
ALEXSTONE <solo-drive@bk.ru>
2025-07-06 11:40:25 +0300

    hw_40 - добавил модели в Админку

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Service, Master, Order, Review
 
-
+admin.site.register(Service)
+admin.site.register(Master)
+admin.site.register(Order)
+admin.site.register(Review)
