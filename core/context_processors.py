afad6c0759c586adc26b7d83b7c24c51d54c6d5e
ALEXSTONE <solo-drive@bk.ru>
2025-07-13 13:15:44 +0300

    hw_40  - переделал почти все! исправил ошибки от Владимира!

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,9 +1,50 @@
-from .models import Service, Master
+# core/context_processors.py
+from django.urls import reverse
+
 
 def menu_items(request):
-    services = Service.objects.all()
-    masters = Master.objects.all()
-    return {
-        'services': services,
-        'masters': masters,
-    }
+    """
+    Контекстный процессор для добавления меню в контекст шаблонов.
+    """
+    menu = [
+        {
+            "name": "Главная",
+            "url": reverse("landing") + "#top",
+            "icon_class": "bi-house",
+        },
+        {
+            "name": "Мастера",
+            "url": reverse("landing") + "#masters",
+            "icon_class": "bi-person-badge",
+        },
+        {
+            "name": "Услуги",
+            "url": reverse("landing") + "#services",
+            "icon_class": "bi-scissors",
+        },
+        {
+            "name": "Отзывы",
+            "url": reverse("landing") + "#reviews",
+            "icon_class": "bi-chat-dots",
+        },
+        {
+            "name": "Записаться",
+            "url": reverse("landing") + "#get-order",
+            "icon_class": "bi-calendar-check",
+        },
+    ]
+
+    staff_menu = [
+        {
+            "name": "Заявки",
+            "url": reverse("orders"),
+            "icon_class": "bi-clipboard-data",
+        },
+        # {
+        #     "name": "Список услуг",
+        #     "url": reverse("services-list"),
+        #     "icon_class": "bi-list-check",
+        # },
+    ]
+
+    return {"menu_items": menu, "menu_staff_items": staff_menu}
